9fe01d8b048ce6d494b4ccea7cb0f8edb57d9dc6
KitsuneNoctus <theceintiant@gmail.com>
2019-09-27 16:42:09 -0700

    Comment form added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 
 # host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+#Advice from Zain Raza
 host = os.environ.get('MONGODB_URI')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
