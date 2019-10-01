3dfb96725ee983830521a8e6e42b44e5a86ed5ef
KitsuneNoctus <theceintiant@gmail.com>
2019-10-01 13:56:18 -0700

    minor change

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 # host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 #Advice from Zain Raza
-host = os.environ.get('MONGODB_URI')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
